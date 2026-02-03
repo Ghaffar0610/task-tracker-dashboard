033f4bc2faf3c8232489e6c21bc116ad21978bc4
Ghaffar0610 <ghaffarbwp786@gmail.com>
2026-02-03 18:06:49 +0500

    update pages and added routes

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,8 @@
+import {Link} from 'react-router-dom';
+const Sidebar =()=> {
+    return (
+        <div className='w-64 h-screen bg-blue-600 text-white flex-col p-5'>
+            
+        </div>
+    )
+}
---
+++ task-tracker/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+function Login() {
+  return (
+    <div className="page">
+      <h1>Login</h1>
+      <p>Sign in to access your dashboard.</p>
+    </div>
+  );
+}
+
+export default Login;
+
